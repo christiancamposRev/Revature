ccf986395ae4ba5bc21e089d2107071531bc33cb
Christian Campos <christian278@revature.net>
2022-08-17 14:10:17 -0700

    SQL

--- CLASS/exercises GIT/Week 3/SQL Practice.sql
+++ CLASS/exercises GIT/Week 3/SQL Practice.sql
@@ -1,19 +1,23 @@
 select * from "Employee"; 
 select * from "Employee" where "LastName" = 'King';
-select * from "Employee" where "FirstName" 'Andrew' and "LastName" 'REPORTSTO is NULL' ;
+select * from "Employee" where  "FirstName" = 'Andrew' and "ReportsTo" is Null;
 select * from "Album" order by "Title" desc; 
-select * from "first name" from "Customer"order by "city" asc; 
-select * "Invoices" where "Billing Adresses" like "T%";
-select * from "Invoices" where "Total" between 15 and 50; 
-select * from "Employee" where "Hiredate" between '2003 -06-01' and '2004-03-01'; 
+select "FirstName" from "Customer" order by "City" asc; 
+select * from "Invoice" where "BillingAddress" like 'T%';
+select * from "Invoice" where "Total" between 15 and 50; 
+select * from "Employee" where "HireDate" between '2003-06-01' and '2004-03-01'; 
 
 
 
-INSERT INTO "Genre" ("GenreId", "Name") VALUES (26, N'Latin Trap')
-INSERT INTO "Genre" ("GenreId", "Name") VALUES (27, N'Alternative Rock')
-INSERT INTO "Employee" ("EmployeeId", "LastName", "FirstName", "Title", "ReportsTo", "BirthDate", "HireDate", "Address", "City", "State", "Country", "PostalCode", "Phone", "Fax", "Email") VALUES (9, N'Peter', N'Parker',N'Superhero', 1, '03/15/1996', '2012/5/1', N'Null', N'Null', N'Null', N'USA', N'Null', N'Null', N'Null (uNll', N'Null');
-INSERT INTO "Employee" ("EmployeeId", "LastName", "FirstName", "Title", "ReportsTo", "BirthDate", "HireDate", "Address", "City", "State", "Country", "PostalCode", "Phone", "Fax", "Email") VALUES (10, N'Bruce', N'Wayne',N'Superhero', 1, '03/15/1996', '2012/5/1', N'Null', N'Null', N'Null', N'USA', N'Null', N'Null', N'Null (uNll', N'Null');
-INSERT INTO "Customer" ("CustomerId", "FirstName", "LastName", "Company","Address", "City", "State", "Country", "PostalCode", "Phone", "Fax", "Email", "SupportRepId") VALUES (60, N'Jason', N'Todd', N'Null', N'Null, N'Null', N'null', N'Null', N'Null', N'Null', N'Null', N'Null',N'Null', Null);
-INSERT INTO "Customer" ("CustomerId", "FirstName", "LastName", "Company","Address", "City", "State", "Country", "PostalCode", "Phone", "Fax", "Email", "SupportRepId") VALUES (61, N'Robin', N'Grayson', N'Null', N'Null, N'Null', N'null', N'Null', N'Null', N'Null', N'Null', N'Null',N'Null', 'Null');
+insert into "Genre" ("GenreId", "Name") values (26, N'Latin Trap');
+insert into "Genre" ("GenreId", "Name") values (27, N'Alternative Rock');
+
+insert into "Employee" ("EmployeeId", "LastName", "FirstName", "Title", "ReportsTo", "BirthDate", "HireDate", "Address", "City", "State", "Country", "PostalCode", "Phone", "Fax", "Email") VALUES (9, N'Peter', N'Parker',N'Superhero', 1, '03/15/1996', '2012/5/1', N'Null', N'Null', N'Null', N'USA', N'Null', N'Null', N'Null (uNll', N'Null');
+insert into "Employee" ("EmployeeId", "LastName", "FirstName", "Title", "ReportsTo", "BirthDate", "HireDate", "Address", "City", "State", "Country", "PostalCode", "Phone", "Fax", "Email") VALUES (10, N'Bruce', N'Wayne',N'Superhero', 1, '03/15/1996', '2012/5/1', N'Null', N'Null', N'Null', N'USA', N'Null', N'Null', N'Null (uNll', N'Null');
+
+insert into "Customer" ("CustomerId", "FirstName", "LastName", "Company","Address", "City", "State", "Country", "PostalCode", "Phone", "Fax", "Email", "SupportRepId") VALUES (60, N'Saul', N'Goodman',N'Null',N'Null', N'Null', N'Null', N'Null', N'Null', N'Null', N'Null', N'Null',6)
+
+insert into "Customer" ("CustomerId", "FirstName", "LastName", "Company","Address", "City", "State", "Country", "PostalCode", "Phone", "Fax", "Email", "SupportRepId") VALUES (61, N'Gus', N'Fring', N'Null',N'Null', N'Null', N'Null', N'Null', N'Null', N'Null', N'Null', N'Null',6)
+
 UPDATE "Customer" SET "FirstName" = 'Robert' , "LastName" = 'Walter' where "LastName" = 'Mitchell';
-UPDATE"Artist" SET "Name" = 'Creedence Clearwater Revival' where "Name" = "CCR"
+UPDATE"Artist" SET "Name" = 'Creedence Clearwater Revival' where "Name" = 'CCR'
