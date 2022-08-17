244ea12392e7efcd0d09610d8ce193fe290418e7
Christian Campos <christian278@revature.net>
2022-08-17 09:29:22 -0700

    SQL

--- CLASS/exercises GIT/Week 3/SQL Practice.sql
+++ CLASS/exercises GIT/Week 3/SQL Practice.sql
@@ -1,3 +1,14 @@
+select * from "Employee"; 
+select * from "Employee" where "LastName" = 'King';
+select * from "Employee" where "FirstName" 'Andrew' and "LastName" 'REPORTSTO is NULL' ;
+select * from "Album" order by "Title" desc; 
+select * from "first name" from "Customer"order by "city" asc; 
+select * "Invoices" where "Billing Adresses" like "T%";
+select * from "Invoices" where "Total" between 15 and 50; 
+select * from "Employee" where "Hiredate" between '2003 -06-01' and '2004-03-01'; 
+
+
+
 INSERT INTO "Genre" ("GenreId", "Name") VALUES (26, N'Latin Trap')
 INSERT INTO "Genre" ("GenreId", "Name") VALUES (27, N'Alternative Rock')
 INSERT INTO "Employee" ("EmployeeId", "LastName", "FirstName", "Title", "ReportsTo", "BirthDate", "HireDate", "Address", "City", "State", "Country", "PostalCode", "Phone", "Fax", "Email") VALUES (9, N'Peter', N'Parker',N'Superhero', 1, '03/15/1996', '2012/5/1', N'Null', N'Null', N'Null', N'USA', N'Null', N'Null', N'Null (uNll', N'Null');
