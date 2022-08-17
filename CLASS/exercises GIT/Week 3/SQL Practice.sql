abe67b85f4dbfafd0b220a575568cc6e230850af
Christian Campos <christian278@revature.net>
2022-08-16 23:12:35 -0700

    SQL

---
+++ CLASS/exercises GIT/Week 3/SQL Practice.sql
@@ -0,0 +1,8 @@
+INSERT INTO "Genre" ("GenreId", "Name") VALUES (26, N'Latin Trap')
+INSERT INTO "Genre" ("GenreId", "Name") VALUES (27, N'Alternative Rock')
+INSERT INTO "Employee" ("EmployeeId", "LastName", "FirstName", "Title", "ReportsTo", "BirthDate", "HireDate", "Address", "City", "State", "Country", "PostalCode", "Phone", "Fax", "Email") VALUES (9, N'Peter', N'Parker',N'Superhero', 1, '03/15/1996', '2012/5/1', N'Null', N'Null', N'Null', N'USA', N'Null', N'Null', N'Null (uNll', N'Null');
+INSERT INTO "Employee" ("EmployeeId", "LastName", "FirstName", "Title", "ReportsTo", "BirthDate", "HireDate", "Address", "City", "State", "Country", "PostalCode", "Phone", "Fax", "Email") VALUES (10, N'Bruce', N'Wayne',N'Superhero', 1, '03/15/1996', '2012/5/1', N'Null', N'Null', N'Null', N'USA', N'Null', N'Null', N'Null (uNll', N'Null');
+INSERT INTO "Customer" ("CustomerId", "FirstName", "LastName", "Company","Address", "City", "State", "Country", "PostalCode", "Phone", "Fax", "Email", "SupportRepId") VALUES (60, N'Jason', N'Todd', N'Null', N'Null, N'Null', N'null', N'Null', N'Null', N'Null', N'Null', N'Null',N'Null', Null);
+INSERT INTO "Customer" ("CustomerId", "FirstName", "LastName", "Company","Address", "City", "State", "Country", "PostalCode", "Phone", "Fax", "Email", "SupportRepId") VALUES (61, N'Robin', N'Grayson', N'Null', N'Null, N'Null', N'null', N'Null', N'Null', N'Null', N'Null', N'Null',N'Null', 'Null');
+UPDATE "Customer" SET "FirstName" = 'Robert' , "LastName" = 'Walter' where "LastName" = 'Mitchell';
+UPDATE"Artist" SET "Name" = 'Creedence Clearwater Revival' where "Name" = "CCR"
